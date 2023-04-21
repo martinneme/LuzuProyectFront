094be173f26ba73f016ea9273c75dc2faa7b97f0
mneme07 <mjneme1995@gmail.com>
2023-04-20 21:14:45 -0300

    refactor wpp cel

--- src/components/channels.jsx
+++ src/components/channels.jsx
@@ -10,7 +10,7 @@ function NavChannels() {
       <Card className="iconButton"><a href="https://www.youtube.com/c/luzutv"><i className="bi bi-youtube yt"></i></a></Card>
       <Card className="iconButton"><a href="https://twitch.tv/luzu_tv"><i className="bi bi-twitch twch"></i></a></Card>
       <Card className="iconButton"><a href="https://www.instagram.com/luzutv"><i className="bi bi-instagram ig"></i></a></Card>
-      <Card className="iconButton"><a href="https://wa.me/1154740668"><i className="bi bi-whatsapp wpp"></i></a></Card>
+      <Card className="iconButton"><a href="https://wa.me/+541154740668"><i className="bi bi-whatsapp wpp"></i></a></Card>
      </Card>
     </Card>
   );
